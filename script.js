c3c54bd7f85a63d8353e086939c657d1ab012bf7
artem <artemfr@hotmail.com>
2023-12-01 12:05:32 +0100

    Added fetchdata function and a javascript file

---
+++ script.js
@@ -0,0 +1,10 @@
+function fetchData() {
+    const sampleData = [
+        { country: 'Country A', divorceRate: 2.5 },
+        { country: 'Country B', divorceRate: 3.7 },
+        { country: 'Country C', divorceRate: 1.8 },
+    ];
+
+    // Display the data
+    displayData(sampleData);
+}
